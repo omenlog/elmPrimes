be39b93aeadd23045db859bc85b3279ba87b201f
tuxjs <omarlg@unica.cu>
2016-11-13 23:25:39 -0500

    Adding maybe to primesLessThan function

--- primes.elm
+++ primes.elm
@@ -1,72 +1,93 @@
+module Main exposing (..)
+
 import Html exposing (text)
 
-checkPrimalityOf: Int -> Int -> Int -> Bool
+
+checkPrimalityOf : Int -> Int -> Int -> Bool
 checkPrimalityOf x y root =
-    if ( x <= 1) then
+    if (x <= 1) then
         False
     else if (x == 2) then
         True
     else if (rem x 2 == 0) then
         False
-    else if ( y > root) then
+    else if (y > root) then
         True
     else if (rem x y == 0) then
         False
     else
         checkPrimalityOf x (y + 2) root
 
-squareRoot: Int -> Int
+
+squareRoot : Int -> Int
 squareRoot x =
-  toFloat x |> sqrt |> floor
+    toFloat x |> sqrt |> floor
 
-createSieve: number -> List number
+
+createSieve : number -> List number
 createSieve x =
-  [2..(x - 1)]
+    [2..(x - 1)]
 
-isPrime: Int -> Bool
+
+isPrime : Int -> Bool
 isPrime x =
     checkPrimalityOf x 3 (squareRoot x)
 
-removeMultiplesOf: Int -> List Int -> List Int
-removeMultiplesOf x sieve  =
-  let
-    begin = List.filter (\n-> n > x && n < x*x) sieve
-    end = List.filter (\n-> n > x*x ) sieve
-  in
-    begin ++ (removeMultiples x end)
 
-removeMultiples: Int -> List Int -> List Int
-removeMultiples prime sieve =
-  case sieve of
-    [] ->
-      []
-    (x::xs) ->
-      if ( rem x prime /= 0) then
-        x :: removeMultiples prime xs
-      else
-        removeMultiples prime xs
+removeMultiplesOf : Int -> List Int -> List Int
+removeMultiplesOf x sieve =
+    let
+        begin =
+            List.filter (\n -> n > x && n < x * x) sieve
 
-findPrimesIn: List Int -> List Int
+        end =
+            List.filter (\n -> n > x * x) sieve
+    in
+        begin ++ (removeMultiples x end)
+
+
+removeMultiples : Int -> List Int -> List Int
+removeMultiples prime sieveEnd =
+    case sieveEnd of
+        [] ->
+            []
+
+        x :: xs ->
+            if (rem x prime /= 0) then
+                x :: removeMultiples prime xs
+            else
+                removeMultiples prime xs
+
+
+findPrimesIn : List Int -> List Int
 findPrimesIn sieve =
-  case sieve of
-    ([]) ->
-        []
-    (x::_)->
-      if ((isPrime x)) then
-          let
-            newSieve = removeMultiplesOf x sieve
-          in
-            [x] ++ (findPrimesIn newSieve)
-      else
-          findPrimesIn (List.drop 1 sieve)
+    case sieve of
+        [] ->
+            []
 
-primesLessThan: Int -> List Int
+        x :: _ ->
+            if ((isPrime x)) then
+                let
+                    newSieve =
+                        removeMultiplesOf x sieve
+                in
+                    [ x ] ++ (findPrimesIn newSieve)
+            else
+                findPrimesIn (List.drop 1 sieve)
+
+
+primesLessThan : Int -> Maybe (List Int)
 primesLessThan x =
-  let
-    sieve = createSieve x
-  in
-  findPrimesIn sieve
+    if (x <= 2) then
+        Nothing
+    else
+        let
+            sieve =
+                createSieve x
+        in
+            Just (findPrimesIn sieve)
 
-main: Html.Html a
+
+main : Html.Html a
 main =
-  text  ( primesLessThan 1000 |> toString)
+    text (primesLessThan 1000 |> toString)
